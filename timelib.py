11d91493ee9a1b02e8182d7b4bb656280580a8ca
Tanaka Ikko <tanaka@example.com>
2020-09-26 17:51:55 +0900

    途中

--- timelib.py
+++ timelib.py
@@ -1,4 +1,14 @@
-import datetime
+import datetime,json
 
 
-def dayslist()
+dt_now = datetime.datetime.now()
+
+print(dt_now.strftime('%Y/%m/%d/ %H:%M:%S'))
+
+
+def input_json(json_name):
+    with open(json_name) as f:
+        para_list = json.load(f)
+    x = para_list['x']
+    delta = para_list['delta']
+    conv = para_list['conv']
